4517aad229a0cb33efb484a1e2cfeac7a1e372e1
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-14 16:56:49 +0900

    Create public_subnet.tf

---
+++ network/public_subnet.tf
@@ -0,0 +1,21 @@
+resource "aws_subnet" "tf_pub_sub_1" {
+  vpc_id                 = aws_vpc.tf_vpc.id
+  cidr_block             = "10.0.1.0/24"  
+  availability_zone      = "ap-northeast-2a"
+  map_public_ip_on_launch = true  # 퍼블릭 IP 주소 자동 할당 : 활성화
+
+  tags = {
+    Name = "tf_pub_sub_1"
+  }
+}
+
+resource "aws_subnet" "tf_pub_sub_1" {
+  vpc_id                 = aws_vpc.tf_vpc.id
+  cidr_block             = "10.0.2.0/24" 
+  availability_zone      = "ap-northeast-2c"
+  map_public_ip_on_launch = true # 퍼블릭 IP 주소 자동 할당 : 활성화
+
+  tags = {
+    Name = "tf_pub_sub_2"
+  }
+}
