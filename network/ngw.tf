a4a77e816d114a2c0a3c459250a1644d99471d0c
HJ <heejnam0918@gmail.com>
2025-02-15 20:10:47 +0900

    Update ngw.tf

--- network/ngw.tf
+++ network/ngw.tf
@@ -9,7 +9,6 @@ resource "aws_eip" "tf_eip" {
 resource "aws_nat_gateway" "tf_nat" {
   allocation_id = aws_eip.tf_eip.allocation_id
   subnet_id     = aws_subnet.tf_pub_sub_2.id
-  depends_on = [aws_eip.tf_eip]
 
   tags = {
     Name = "tf_nat"
