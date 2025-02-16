e23a177a788995ba6906363a66061777f6da0e1c
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-16 10:39:28 +0900

    Update ngw.tf

--- network/ngw.tf
+++ network/ngw.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "tf_eip" {
 }
 
 resource "aws_nat_gateway" "tf_nat" {
-  allocation_id = aws_eip.tf_eip.allocation_id
+  allocation_id = aws_eip.tf_eip.allocation_id  # aws_eip.tf_eip.id ??
   subnet_id     = aws_subnet.tf_pub_sub_2.id
 
   tags = {
