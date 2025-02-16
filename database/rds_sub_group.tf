b2852c9ca8f49c8ce376ea785ba4b58f64e948a6
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-16 21:35:20 +0900

    Update rds_sub_group.tf

--- database/rds_sub_group.tf
+++ database/rds_sub_group.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "tf_rds_subnet_group" {
   name       = "tf_rds_subnet_group"
-  subnet_ids = [aws_subnet.rds_sub_1.id, aws_subnet.rds_sub_2.id]
+  subnet_ids = [aws_subnet.tf_rds_sub_1.id, aws_subnet.tf_rds_sub_2.id]
 
   tags = {
     Name = "tf_rds_subnet_group"
