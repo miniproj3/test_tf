47781eab98c5172604645afb6c0b18e62f3d3bfd
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-16 11:11:53 +0900

    Update and rename bastion_sg to bastion_sg.tf

---
+++ compute/bastion_sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "tf_bastion_sg" {
+  name   = "tf_bastion_sg"
+  vpc_id = aws_vpc.tf_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 외부에서 SSH 접속 허용
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf_bastion_sg"
+  }
+}
