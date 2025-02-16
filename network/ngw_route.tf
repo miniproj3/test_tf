623f93dc2ad14be926698d4090ea0b94546b049c
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-16 10:47:12 +0900

    Update ngw_route.tf

--- network/ngw_route.tf
+++ network/ngw_route.tf
@@ -1,12 +1,17 @@
 resource "aws_route_table" "tf_pri_rtb" {
   vpc_id = aws_vpc.tf_vpc.id
 
+  route {  # route 추가
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.tf_nat.id
+  }
+
   tags = {
     Name = "tf_pri_rtb"
   }
 }
 
-resource "aws_route" "tf_pri_route_nat" {
+resource "aws_route" "tf_pri_route_nat" {   # 위에 route와 동일한 방식
   route_table_id = aws_route_table.tf_pri_rtb.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.tf_nat.id
