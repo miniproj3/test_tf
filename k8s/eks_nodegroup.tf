601a47790670e77408e5617b95bb9334ceebf3c5
HJ <heejnam0918@gmail.com>
2025-02-17 15:35:50 +0900

    Update eks_nodegroup.tf

--- k8s/eks_nodegroup.tf
+++ k8s/eks_nodegroup.tf
@@ -1,1 +1,68 @@
+# AWS eks_node_group Resource
+resource "aws_eks_node_group" "tf_eks_managed_node_group" {
+  cluster_name    = aws_eks_cluster.tf_eks_cluster.name
+  node_group_name = "tf-eks-managed-node-group"
+  node_role_arn   = aws_iam_role.tf_eks_managed_node_role.arn
+  subnet_ids      = [aws_subnet.tf_pri_sub_1.id, aws_subnet.tf_pri_sub_2.id]
 
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
+  }
+
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"  # Amazon Linux 2 AMI
+  disk_size      = 20
+  capacity_type  = "ON_DEMAND"
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+tags = {
+    Name = "tf_eks_managed_node_group"
+  }
+}
+
+
+# 노드 그룹 IAM 역할
+resource "aws_iam_role" "tf_eks_managed_node_group_iam_role" {
+  name = "tf-eks-managed-node-role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+
+# 노드 그룹이 EKS 클러스터와 상호작용할 수 있도록 필요한 정책 연결
+resource "aws_iam_role_policy_attachment" "tf_eks_managed_node_group_policy_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "tf_eks_managed_node_group_policy_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_iam_role_policy_attachment" "etf_eks_managed_node_group_policy_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.example.name
+}
