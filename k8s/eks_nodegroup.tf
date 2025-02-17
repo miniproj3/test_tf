88d47afde6f17d523fe3f89727db22fbd55a9c54
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-17 16:03:52 +0900

    Update eks_nodegroup.tf

--- k8s/eks_nodegroup.tf
+++ k8s/eks_nodegroup.tf
@@ -4,6 +4,7 @@ resource "aws_eks_node_group" "tf_eks_managed_node_group" {
   node_group_name = "tf-eks-managed-node-group"
   node_role_arn   = aws_iam_role.tf_eks_managed_node_role.arn
   subnet_ids      = [aws_subnet.tf_pri_sub_1.id, aws_subnet.tf_pri_sub_2.id]
+  security_groups = [aws_security_group.tf_eks_node_group_sg.id]
 
   scaling_config {
     desired_size = 2
