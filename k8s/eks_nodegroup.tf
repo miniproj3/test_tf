499c055342103be00b5a1198679c0dd30c79a1d4
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-17 17:25:41 +0900

    Update eks_nodegroup.tf

--- k8s/eks_nodegroup.tf
+++ k8s/eks_nodegroup.tf
@@ -1,21 +1,32 @@
 # AWS eks_node_group Resource
+resource "aws_launch_template" "tf_eks_node_lt" {
+  name_prefix   = "tf-eks-node-lt"
+  image_id      = "ami-0a20b1b99b215fb27" # Linux 2 ami
+  instance_type = "t3.medium"
+  network_interfaces {
+    security_groups             = [aws_security_group.tf_eks_node_group_sg.id]
+  }
+}
+
 resource "aws_eks_node_group" "tf_eks_managed_node_group" {
   cluster_name    = aws_eks_cluster.tf_eks_cluster.name
   node_group_name = "tf-eks-managed-node-group"
   node_role_arn   = aws_iam_role.tf_eks_managed_node_role.arn
   subnet_ids      = [aws_subnet.tf_pri_sub_1.id, aws_subnet.tf_pri_sub_2.id]
-  security_groups = [aws_security_group.tf_eks_node_group_sg.id]
+
+  launch_template {
+    id      = aws_launch_template.tf_eks_node_lt.id
+    version = "$Latest"
+  }
+
+  disk_size      = 20
+  capacity_type  = "ON_DEMAND"
 
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 2
   }
-
-  instance_types = ["t3.medium"]
-  ami_type       = "AL2_x86_64"  # Amazon Linux 2 AMI
-  disk_size      = 20
-  capacity_type  = "ON_DEMAND"
 
   update_config {
     max_unavailable = 1
