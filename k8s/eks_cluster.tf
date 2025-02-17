a66df5dafc00778bdcb3243159891da8fe91389e
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-17 16:16:41 +0900

    Update eks_cluster.tf

--- k8s/eks_cluster.tf
+++ k8s/eks_cluster.tf
@@ -1,7 +1,7 @@
 # EKS Cluster
 resource "aws_eks_cluster" "tf_eks_cluster" {
   name = "tf-eks-cluster"                            
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = aws_iam_role.tf_eks_cluster_role.arn
   
   vpc_config {                                    
     subnet_ids = [aws_subnet.tf_pri_sub_1.id, aws_subnet.tf_pri_sub_2.id]
