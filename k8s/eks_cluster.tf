9902fa95d51df67546e7f51c554c05fdccd25aab
HJ <heejnam0918@gmail.com>
2025-02-17 15:17:10 +0900

    Update eks_cluster.tf

--- k8s/eks_cluster.tf
+++ k8s/eks_cluster.tf
@@ -4,22 +4,14 @@ resource "aws_eks_cluster" "tf_eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
   
   vpc_config {                                    
-
-    subnet_ids              = [                  
-      aws_subnet.tf_pri_sub_1.id,
-      aws_subnet.tf_pri_sub_2.id
-    ]
-
-    endpoint_public_access  = false               
-    endpoint_private_access = true             
-
-    ]
+    subnet_ids = [aws_subnet.tf_pri_sub_1.id, aws_subnet.tf_pri_sub_2.id]
+    endpoint_public_access  = false
+    endpoint_private_access = true
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.tf_eks_cluster_policy_AmazonEKSClusterPolicy
-  ]
-  
+  depends_on = [aws_iam_role_policy_attachment.tf_eks_cluster_policy_AmazonEKSClusterPolicy]
+  security_group_ids = [aws_security_group.tf_eks_cluster_sg.id]
+
   tags = {
     Name = tf_eks_cluster
   }
@@ -28,7 +20,7 @@ resource "aws_eks_cluster" "tf_eks_cluster" {
 
 # EKS 클러스터 IAM 역할
 resource "aws_iam_role" "tf_eks_cluster_iam_role" {
-  name = "tf_eks-cluster-iam-role"
+  name = "tf-eks-cluster-iam-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
