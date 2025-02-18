e502b8dfaf9ac746826ce98ca2a5064631e81e2f
A-Hyeon <117140125+ahastuart@users.noreply.github.com>
2025-02-18 09:47:43 +0900

    Delete database directory

--- database/rds_sub_group.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_db_subnet_group" "tf_rds_subnet_group" {
-  name       = "tf_rds_subnet_group"
-  subnet_ids = [aws_subnet.tf_rds_sub_1.id, aws_subnet.tf_rds_sub_2.id]
-
-  tags = {
-    Name = "tf_rds_subnet_group"
-  }
-}
